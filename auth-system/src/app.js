2ad43e373380e0dc9a00b74c3dd533fd8149f13e
mabas <johnmabas1@gmail.com>
2026-09-20 00:31:16 +0100

    auth system

--- auth-system/src/app.js
+++ auth-system/src/app.js
@@ -6,10 +6,8 @@ const { notFound, errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
-// Parse incoming JSON request bodies (e.g. { name, email, ... })
 app.use(express.json());
 
-// A tiny hint so /, /api etc. do not feel dead
 app.get('/', (req, res) => {
   res.json({
     success: true,
@@ -17,11 +15,9 @@ app.get('/', (req, res) => {
   });
 });
 
-// Mount the route groups
 app.use('/api/auth', authRoutes);
 app.use('/api', userRoutes);
 
-// 404 for any unknown route, then the central error handler
 app.use(notFound);
 app.use(errorHandler);
 
